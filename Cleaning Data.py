d7148291f25b7aec18b5ea71a15869514fc8bdfe
JiatongGao <travisgao58@gmail.com>
2023-05-27 14:07:25 -0700

    Add code

--- Cleaning Data.py
+++ Cleaning Data.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import numpy as np
 df = pd.read_csv("weatherAUS.csv")
 # Deal with NA
-missing_values = df.isnull().sum() 
+# Replace NaN with the mean value of that column
+missing_values = df.isnull().sum()
+print(missing_values)
+df_num = df.apply(pd.to_numeric, errors='coerce')
+df_num = df_num.fillna(df_num.mean())
+df_num[['Date','Location','WindGustDir','WindDir9am','WindDir3pm','RainToday','RainTomorrow']] =\
+df[['Date','Location','WindGustDir','WindDir9am','WindDir3pm','RainToday','RainTomorrow']]
+df = df_num
+df = df.drop(['WindDir9am','WindSpeed9am','Humidity9am','Pressure9am','Cloud9am','Temp9am'],axis=1)
+df['Date'] = pd.to_datetime(df['Date'])# Convert date form
+print(df)
+
