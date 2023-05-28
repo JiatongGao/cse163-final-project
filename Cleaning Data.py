592c145f2f54d5866d3b60f776117402570decbe
Cloudsy55 <yingyw2@uw.edu>
2023-05-27 17:58:34 -0700

    add code

--- Cleaning Data.py
+++ Cleaning Data.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
 df = pd.read_csv("weatherAUS.csv")
 # Deal with NA
 # Replace NaN with the mean value of that column
@@ -10,7 +13,35 @@ df_num = df_num.fillna(df_num.mean())
 df_num[['Date','Location','WindGustDir','WindDir9am','WindDir3pm','RainToday','RainTomorrow']] =\
 df[['Date','Location','WindGustDir','WindDir9am','WindDir3pm','RainToday','RainTomorrow']]
 df = df_num
-df = df.drop(['WindDir9am','WindSpeed9am','Humidity9am','Pressure9am','Cloud9am','Temp9am'],axis=1)
 df['Date'] = pd.to_datetime(df['Date'])# Convert date form
 print(df)
 
+# Split the data into features (X) and target variable (y)
+min = df['MinTemp']
+max = df['MaxTemp']
+average = (min + max) / 2
+df['Temperature'] = average
+data = df.loc[:, ['Temperature', 'Rainfall', 'Evaporation','Sunshine','WindGustDir', 'WindGustSpeed', 'Location', 'Date']]
+data = data.loc[data.rows == 'Albury', :]
+X = data.drop('Temperature', axis=1)  # Features matrix
+y = data['Temperature']  # Target variable
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Fit a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Get the feature importance scores
+feature_importance = np.abs(model.coef_)
+
+# Create a DataFrame to store the factors and their importance scores
+importance_df = pd.DataFrame({'Factor': X.columns, 'Importance': feature_importance})
+importance_df = importance_df.sort_values(by='Importance', ascending=False)
+
+# Print the factors and their importance scores
+print(importance_df)
+
+# Determine the factor with the strongest influence
+strongest_factor = importance_df['Factor'].iloc[0]
