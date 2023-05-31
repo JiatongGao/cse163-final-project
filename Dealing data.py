317e193c694fe6699f7af33d821debdd482a5405
JiatongGao <travisgao58@gmail.com>
2023-05-31 02:36:19 -0700

    Lasso half finish

--- Dealing data.py
+++ Dealing data.py
@@ -1,2 +1,8 @@
-Python 3.11.3 (v3.11.3:f3909b8bc8, Apr  4 2023, 20:12:10) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
+import pandas as pd
+
+data_9am = pd.read_csv("weathernoNA.csv")
+data_9am = data_9am[['Date','Location','Rainfall','Evaporation','Sunshine','WindGustDir',
+                     'WindGustSpeed','WindDir9am','WindSpeed9am','Humidity9am','Pressure9am',
+                     'Cloud9am','Temp9am','RainToday']]
+data_9am.to_csv("weather9am.csv", index=False)
+print(data_9am)
--- Question 2/Q2_LASSO.py
+++ Question 2/Q2_LASSO.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 
 
-# 读取数据集
+
 data = pd.read_csv('weather9am.csv')
 
 encoder = LabelEncoder()
@@ -15,21 +15,21 @@ data['Location'] = encoder.fit_transform(data['Location'])
 data['WindGustDir'] = encoder.fit_transform(data['WindGustDir'])
 data['WindDir9am'] = encoder.fit_transform(data['WindDir9am'])
 data['RainToday'] = encoder.fit_transform(data['RainToday'])
-data['RainTomorrow'] = encoder.fit_transform(data['RainTomorrow'])
+
 
 scaler = MinMaxScaler()
 numeric_features = ['Rainfall', 'Evaporation', 'Sunshine', 'WindGustSpeed',
                     'WindSpeed9am','Humidity9am','Pressure9am','Cloud9am','Temp9am']
 data[numeric_features] = scaler.fit_transform(data[numeric_features])
 
-features = data.drop(['Date', 'Temp9am'], axis=1)
+features = data.drop(['Date','Location','Temp9am'], axis=1)
 target = data['Temp9am']
 split_point = int(len(data)*0.8)
 X_train, X_test = features[:split_point], features[split_point:]
 y_train, y_test = target[:split_point], target[split_point:]
 
 #select an alpha
-alpha_lasso = 10**np.linspace(-3,1,100)
+alpha_lasso = 10**np.linspace(-4,0,100)
 lasso = Lasso()
 coefs_lasso = []
 
@@ -46,5 +46,28 @@ plt.axis('tight')
 plt.xlabel('alpha')
 plt.ylabel('weights: scaled coefficients')
 plt.title('Lasso regression coefficients Vs. alpha')
-plt.legend(data.drop('Temp9am',axis=1, inplace=False).columns)
-plt.show()
+plt.legend(features.columns)
+plt.savefig('alpha_graph_9am.png', bbox_inches='tight')
+
+coefs_list = []
+for i in alpha_lasso:
+    lasso = Lasso(alpha = i)
+    model_lasso = lasso.fit(X_train, y_train)
+    coef = pd.Series(model_lasso.coef_,index=X_train.columns)
+    coefs_list.append(coef[coef != 0].abs().sort_values(ascending=False))
+
+coef_table = pd.concat(coefs_list, axis=1)
+coef_table.columns = alpha_lasso
+coef_table.to_csv('alpha_coef_table_9am.csv', index=True)
+fea = X_train.columns
+a = pd.DataFrame()
+a['feature'] = fea
+a['importance'] = coef.values
+
+a = a.sort_values('importance',ascending = False)
+plt.figure(figsize=(12,8))
+plt.barh(a['feature'],a['importance'])
+plt.title('the importance features')
+plt.savefig('importance_of_feature_9am.png', bbox_inches='tight')
+
+
