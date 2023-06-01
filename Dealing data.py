116fa1af5af256ceb15fc3cf8bea79d2fa1cc6e6
JiatongGao <travisgao58@gmail.com>
2023-06-01 15:54:03 -0700

    nothing

--- Dealing data.py
+++ Dealing data.py
@@ -5,4 +5,10 @@ data_9am = data_9am[['Date','Location','Rainfall','Evaporation','Sunshine','Wind
                      'WindGustSpeed','WindDir9am','WindSpeed9am','Humidity9am','Pressure9am',
                      'Cloud9am','Temp9am','RainToday']]
 data_9am.to_csv("weather9am.csv", index=False)
-print(data_9am)
+
+
+data_3pm = pd.read_csv("weathernoNA.csv")
+data_3pm = data_3pm[['Date','Location','Rainfall','Evaporation','Sunshine','WindGustDir',
+                     'WindGustSpeed','WindDir3pm','WindSpeed3pm','Humidity3pm','Pressure3pm',
+                     'Cloud3pm','Temp3pm','RainToday']]
+data_3pm.to_csv("weather3pm.csv", index=False)
