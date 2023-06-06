bd9c8eef0e0ba7508cf374b55ab40f40d0e803c5
Cloudsy55 <134815228+Cloudsy55@users.noreply.github.com>
2023-06-05 20:49:59 -0700

    Update Question_run_3pm.py

--- Question 3/Question 3 3pm/Question_run_3pm.py
+++ Question 3/Question 3 3pm/Question_run_3pm.py
@@ -1,8 +1,7 @@
-
 from Question_3_predict_3pm import predict_temp_3pm
-import pandas as pd
 import csv
-cities = ['Sydney', 'Melbourne','Brisbane','Perth','Canberra','Adelaide']
+cities = ['Sydney', 'Melbourne', 'Brisbane',
+          'Perth', 'Canberra', 'Adelaide']
 data_3pm = []
 for city in cities:
     mae_ensemble, r2_ensemble, accuracy_ensemble = predict_temp_3pm(city)
@@ -11,10 +10,6 @@ for city in cities:
 csv_file = 'stat_data_3pm.csv'
 with open(csv_file, mode='w', newline='') as file:
     writer = csv.writer(file)
-    
-    # 写入表头
     writer.writerow(['Cities', 'MAE', 'R2', 'Accuracy'])
-    
-    # 写入数据行
     for i in range(len(cities)):
         writer.writerow([cities[i]] + data_3pm[i])
