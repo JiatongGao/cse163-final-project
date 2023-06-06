de54be545e2d17a3c413a0bf1b187559a0a9bf18
Cloudsy55 <134815228+Cloudsy55@users.noreply.github.com>
2023-06-05 20:41:34 -0700

    Update Dealing data.py

--- Dealing data.py
+++ Dealing data.py
@@ -1,14 +1,16 @@
 import pandas as pd
 
 data_9am = pd.read_csv("weathernoNA.csv")
-data_9am = data_9am[['Date','Location','Rainfall','Evaporation','Sunshine','WindGustDir',
-                     'WindGustSpeed','WindDir9am','WindSpeed9am','Humidity9am','Pressure9am',
-                     'Cloud9am','Temp9am','RainToday']]
+data_9am = data_9am[['Date', 'Location', 'Rainfall',
+                     'Evaporation', 'Sunshine', 'WindGustDir',
+                     'WindGustSpeed', 'WindDir9am', 'WindSpeed9am',
+                     'Humidity9am', 'Pressure9am',
+                     'Cloud9am', 'Temp9am', 'RainToday']]
 data_9am.to_csv("weather9am.csv", index=False)
-
-
 data_3pm = pd.read_csv("weathernoNA.csv")
-data_3pm = data_3pm[['Date','Location','Rainfall','Evaporation','Sunshine','WindGustDir',
-                     'WindGustSpeed','WindDir3pm','WindSpeed3pm','Humidity3pm','Pressure3pm',
-                     'Cloud3pm','Temp3pm','RainToday']]
+data_3pm = data_3pm[['Date', 'Location', 'Rainfall', 'Evaporation',
+                     'Sunshine', 'WindGustDir',
+                     'WindGustSpeed', 'WindDir3pm', 'WindSpeed3pm',
+                     'Humidity3pm', 'Pressure3pm',
+                     'Cloud3pm', 'Temp3pm', 'RainToday']]
 data_3pm.to_csv("weather3pm.csv", index=False)
