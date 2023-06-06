33f5e2469edf8a6c9616490e1f65110781b90424
Cloudsy55 <134815228+Cloudsy55@users.noreply.github.com>
2023-06-05 20:52:17 -0700

    Update Q3_run_9am.py

--- Question 3/Question 3 9am/Q3_run_9am.py
+++ Question 3/Question 3 9am/Q3_run_9am.py
@@ -1,8 +1,7 @@
-
 from Question_3_predict_9am import predict_temp_9am
-import pandas as pd
 import csv
-cities = ['Sydney', 'Melbourne','Brisbane','Perth','Canberra','Adelaide']
+cities = ['Sydney', 'Melbourne', 'Brisbane',
+          'Perth', 'Canberra', 'Adelaide']
 data_9am = []
 for city in cities:
     mae_ensemble, r2_ensemble, accuracy_ensemble = predict_temp_9am(city)
@@ -11,11 +10,6 @@ for city in cities:
 csv_file = 'stat_data_9am.csv'
 with open(csv_file, mode='w', newline='') as file:
     writer = csv.writer(file)
-    
-    # 写入表头
     writer.writerow(['Cities', 'MAE', 'R2', 'Accuracy'])
-    
-    # 写入数据行
     for i in range(len(cities)):
         writer.writerow([cities[i]] + data_9am[i])
-    
