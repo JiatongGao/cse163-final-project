f8631f12a420603cd996670979b579332d26eaec
Cloudsy55 <134815228+Cloudsy55@users.noreply.github.com>
2023-06-05 20:57:57 -0700

    Update Question_3_predict_9am.py

--- Question 3/Question 3 9am/Question_3_predict_9am.py
+++ Question 3/Question 3 9am/Question_3_predict_9am.py
@@ -12,14 +12,17 @@ import matplotlib.pyplot as plt
 import warnings
 
 
-
 def predict_temp_9am(city):
     warnings.filterwarnings("ignore")
     df = pd.read_csv("weathernoNA.csv")
-    data = df[df['Location'] == city][['Date', 'Evaporation', 'Rainfall', 'Pressure9am', 'Humidity9am','WindSpeed9am','Temp9am']]
-    features = data[['Evaporation', 'Rainfall', 'Pressure9am', 'Humidity9am','WindSpeed9am']].values
+    data = df[df['Location'] == city][['Date', 'Evaporation',
+                                       'Rainfall', 'Pressure9am',
+                                       'Humidity9am', 'WindSpeed9am',
+                                       'Temp9am']]
+    features = data[['Evaporation', 'Rainfall',
+                     'Pressure9am', 'Humidity9am', 'WindSpeed9am']].values
     target = data[['Temp9am']].values
-    # standarized 
+    # standarized
     scaler = MinMaxScaler()
     features = scaler.fit_transform(features)
 
@@ -35,24 +38,25 @@ def predict_temp_9am(city):
     X_train_np = np.array(X_train)
     X_test_np = np.array(X_test)
     y_train_np = np.array(y_train)
-    
 
     # data augment
     def data_augmentation(inputs, labels):
         augmented_inputs = torch.flip(inputs, dims=[0])
         augmented_labels = labels.clone()
-        return torch.cat((inputs, augmented_inputs)), torch.cat((labels, augmented_labels))
+        return torch.cat((inputs, augmented_inputs)), torch.cat(
+                        (labels, augmented_labels))
 
     # Build the neural network model
     class TemperaturePredict(nn.Module):
         def __init__(self, input_size, hidden_size, num_layers):
             super(TemperaturePredict, self).__init__()
-            self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+            self.lstm = nn.LSTM(input_size, hidden_size,
+                                num_layers, batch_first=True)
             self.fc1 = nn.Linear(hidden_size, 128)
             self.relu = nn.ReLU()
             self.fc2 = nn.Linear(128, 64)
             self.fc3 = nn.Linear(64, 1)
-    
+
         def forward(self, x):
             output, _ = self.lstm(x)
             output = output[:, -1, :]
@@ -72,7 +76,8 @@ def predict_temp_9am(city):
 
     # loss
     criterion = nn.MSELoss()
-    optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=weight_decay)
+    optimizer = optim.AdamW(model.parameters(),
+                            lr=0.001, weight_decay=weight_decay)
 
     # train the model
     train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
@@ -81,26 +86,20 @@ def predict_temp_9am(city):
     num_epochs = 100
     for epoch in range(num_epochs):
         epoch_loss = 0.0
-    
         for batch_inputs, batch_labels in train_loader:
-            # data augmentation 
-            batch_inputs, batch_labels = data_augmentation(batch_inputs, batch_labels)
-
+            # data augmentation
+            batch_inputs, batch_labels = data_augmentation(
+                                         batch_inputs, batch_labels)
             optimizer.zero_grad()
-        
             outputs = model(batch_inputs.unsqueeze(1))
             loss = criterion(outputs.squeeze(), batch_labels.squeeze())
-        
             l2_reg = torch.tensor(0.)
             for param in model.parameters():
                 l2_reg += torch.norm(param, p=2)
             loss += weight_decay * l2_reg
-        
             loss.backward()
             optimizer.step()
-
             epoch_loss += loss.item()
-    
     # Use neural network to predict
     model.eval()
     with torch.no_grad():
@@ -108,18 +107,15 @@ def predict_temp_9am(city):
 
     predictions_nn = test_outputs.squeeze().numpy()
     true_labels = y_test_tensor.numpy()
-
-     # Train and test the Random Forest model
-    rf_model = RandomForestRegressor(n_estimators=100, max_depth=10, min_samples_split=2, min_samples_leaf=1)
+# Train and test the Random Forest model
+    rf_model = RandomForestRegressor(n_estimators=100,
+                                     max_depth=10, min_samples_split=2,
+                                     min_samples_leaf=1)
     rf_model.fit(X_train_np, y_train_np)
     rf_predictions = rf_model.predict(X_test_np)
-
-    # Convert the predictions to numpy array
+# Convert the predictions to numpy array
     predictions_rf = np.array(rf_predictions)
-    
-    
-
-    # Ensemble predictions
+# Ensemble predictions
     stacked_features = np.column_stack((predictions_nn, predictions_rf))
     meta_model = LinearRegression()
     meta_model.fit(stacked_features, true_labels)
@@ -130,12 +126,13 @@ def predict_temp_9am(city):
     mae_ensemble = np.mean(absolute_errors_ensemble)
     r2_ensemble = r2_score(true_labels, ensemble_predictions)
     accuracy_ensemble = 100 - (mae_ensemble / np.mean(true_labels)) * 100
-    
     # plot R square
     true_labels_flat = np.ravel(true_labels)
     ensemble_predictions_flat = np.ravel(ensemble_predictions)
-    coefficients = np.polyfit(true_labels_flat, ensemble_predictions_flat, deg=1)
-    x = np.linspace(min(true_labels_flat), max(true_labels_flat), 100)
+    coefficients = np.polyfit(true_labels_flat,
+                              ensemble_predictions_flat, deg=1)
+    x = np.linspace(min(true_labels_flat),
+                    max(true_labels_flat), 100)
     fit_line = np.polyval(coefficients, x)
 
     plt.figure()
