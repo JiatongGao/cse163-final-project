542f95aea6b44fa27d8eab88305c9445bfad1a0e
Cloudsy55 <134815228+Cloudsy55@users.noreply.github.com>
2023-06-05 20:15:19 -0700

    Update Question 1 first version.py

--- Question 1/Question 1 first version.py
+++ Question 1/Question 1 first version.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -13,28 +12,29 @@ df['Date'] = pd.to_datetime(df['Date'])
 df['Year'] = df['Date'].dt.year
 
 df = df[(df['Location'] == city)]
-grouped = df.groupby('Year')['MaxTemp'].agg(['count', lambda x: (x > high_temp).sum()])
+grouped = df.groupby('Year')['MaxTemp'].agg(
+          ['count', lambda x: (x > high_temp).sum()])
 
 grouped['Percentage'] = grouped['<lambda_0>'] / grouped['count'] * 100
-
 grouped.rename(columns={'<lambda_0>': 'AboveTemp'}, inplace=True)
 grouped.reset_index(inplace=True)
 df = pd.merge(df, grouped[['Year', 'Percentage']], on='Year', how='left')
 df = df.loc[:, ['Year', 'Percentage']]
 df_unique = df.drop_duplicates()
 print(df_unique)
-sns.relplot(data=df_unique, x="Year", y="Percentage",kind="line")
+sns.relplot(data=df_unique, x="Year", y="Percentage", kind="line")
 plt.ylabel('Percentage')
-plt.title("Percentage of extreme temperature for " + city + " weather over years")
+plt.title("Percentage of extreme temperature for "
+          + city + " weather over years")
 
 
-#plt.savefig('plot_test.png', bbox_inches='tight')
+# plt.savefig('plot_test.png', bbox_inches='tight')
 
 # Merge the aggregated values back into the original DataFrame
-#df = df.merge(df_agg, on='Year', suffixes=('', '_aggregated'))
-#df_unique = df.drop_duplicates()
-#print(df_unique)
-#sns.relplot(data=df_unique, x="Year", y="MaxTemp_aggregated",kind="line")
-#plt.ylabel('Maximum temperature')
-#plt.title("Maximum temperature for" + city + " weather over years")
-#plt.savefig('plot_test.png', bbox_inches='tight')
+# df = df.merge(df_agg, on='Year', suffixes=('', '_aggregated'))
+# df_unique = df.drop_duplicates()
+# print(df_unique)
+# sns.relplot(data=df_unique, x="Year", y="MaxTemp_aggregated",kind="line")
+# plt.ylabel('Maximum temperature')
+# plt.title("Maximum temperature for" + city + " weather over years")
+# plt.savefig('plot_test.png', bbox_inches='tight')
