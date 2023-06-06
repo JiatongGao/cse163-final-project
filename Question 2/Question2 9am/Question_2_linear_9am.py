e58b65d1934a10b8afaf761f0548c8a5e17efedb
Cloudsy55 <134815228+Cloudsy55@users.noreply.github.com>
2023-06-05 20:38:58 -0700

    Update Question_2_linear_9am.py

--- Question 2/Question2 9am/Question_2_linear_9am.py
+++ Question 2/Question2 9am/Question_2_linear_9am.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("weathernoNA.csv")
-
-
-
-
 # Split the data into features (X) and target variable (y)
-data = df.loc[:, ['Temp9am', 'Rainfall', 'Evaporation','Sunshine', 'WindGustSpeed','Humidity9am','WindSpeed9am','Pressure9am','Cloud9am',]]
+data = df.loc[:, ['Temp9am', 'Rainfall', 'Evaporation',
+                  'Sunshine', 'WindGustSpeed', 'Humidity9am',
+                  'WindSpeed9am', 'Pressure9am', 'Cloud9am',]]
 
 X = data.drop('Temp9am', axis=1)  # Features matrix
 y = data['Temp9am']  # Target variable
@@ -27,7 +24,8 @@ model.fit(X_train, y_train)
 feature_importance = np.abs(model.coef_)
 
 # Create a DataFrame to store the factors and their importance scores
-importance_df = pd.DataFrame({'Factor': X.columns, 'Importance': feature_importance})
+importance_df = pd.DataFrame({'Factor': X.columns,
+                              'Importance': feature_importance})
 importance_df = importance_df.sort_values(by='Importance', ascending=False)
 
 # Print the factors and their importance scores
@@ -39,16 +37,15 @@ print(strongest_factor)
 
 # plot the pie plot
 fig, ax = plt.subplots(figsize=(8, 6))
-explode = [0.1] + [0] * (len(importance_df) - 1)  # Explode the first slice for emphasis
-colors = plt.cm.Set3(np.linspace(0, 1, len(importance_df)))  # Generate a color map for the slices
-
-ax.pie(importance_df['Importance'], labels=importance_df['Factor'], explode=explode,
-       colors=colors, autopct='%1.1f%%', shadow = 'True', startangle=45)
-
-ax.set_title('Correlation of factors and temperature at 9 a.m.',color = 'red')
+explode = [0.1] + [0] * (len(importance_df) - 1)
+# Explode the first slice for emphasis
+colors = plt.cm.Set3(np.linspace(0, 1, len(importance_df)))
+# Generate a color map for the slices
+ax.pie(importance_df['Importance'],
+       labels=importance_df['Factor'], explode=explode,
+       colors=colors, autopct='%1.1f%%', shadow='True', startangle=45)
+ax.set_title('Correlation of factors and temperature at 9 a.m.', color='red')
 ax.legend(loc='best')
 ax.axis('equal')  # Ensure a circular pie chart
-
 plt.tight_layout()  # Adjust spacing between subplots
-
 plt.savefig('factors9am.png', bbox_inches='tight')
